6074a02f2d67e7561de75eb32b2bea3398012501
ViniciusOsvaldo <vinioswaldo92@gmail.com>
2026-03-25 10:10:14 -0300

    Correção de prints

--- Projeto_Breno_Vinicius/main/main.c
+++ Projeto_Breno_Vinicius/main/main.c
@@ -18,27 +18,27 @@ void app_main(void)
     esp_chip_info_t chip_info;
     uint32_t flash_size;
     esp_chip_info(&chip_info);
-    ESP_LOGI(TAG,"This is %s chip with %d CPU core(s), %s%s%s%s, ",
+    ESP_LOGI(TAG,"Este é um microcontrolador %s com %d núcleo(s), %s%s%s%s, ",
            CONFIG_IDF_TARGET,
            chip_info.cores,
            (chip_info.features & CHIP_FEATURE_WIFI_BGN) ? "WiFi/" : "",
            (chip_info.features & CHIP_FEATURE_BT) ? "BT" : "",
            (chip_info.features & CHIP_FEATURE_BLE) ? "BLE" : "",
-           (chip_info.features & CHIP_FEATURE_IEEE802154) ? ", 802.15.4 (Zigbee/Thread)" : "");
+           (chip_info.features & CHIP_FEATURE_IEEE802154) ? ", 802.15.4 (Zigbee/Thread)" : "\n");
 
     unsigned major_rev = chip_info.revision / 100;
     unsigned minor_rev = chip_info.revision % 100;
-    ESP_LOGI(TAG,"silicon revision v%d.%d, ", major_rev, minor_rev);
+    ESP_LOGI(TAG,"silicon revision v%d.%d, \n", major_rev, minor_rev);
     if(esp_flash_get_size(NULL, &flash_size) != ESP_OK) {
-        ESP_LOGE(TAG,"Get flash size failed");
+        ESP_LOGE(TAG,"Falha na Obtenção da Memória Flash!");
         return;
     }
 
-    ESP_LOGI(TAG,"%" PRIu32 "MB %s flash\n", flash_size / (uint32_t)(1024 * 1024),
+    ESP_LOGI(TAG,"Memória Flash: %" PRIu32 "MB %s flash\n", flash_size / (uint32_t)(1024 * 1024),
            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
 
-    ESP_LOGI(TAG,"Minimum free heap size: %" PRIu32 " bytes\n", esp_get_minimum_free_heap_size());
+    ESP_LOGI(TAG,"Free heap: %" PRIu32 " bytes\n", esp_get_minimum_free_heap_size());
 
-    ESP_LOGI(TAG,"ESP-IDF Version: %s\n", IDF_VER);
+    ESP_LOGI(TAG,"Versão do ESP-IDF: %s\n", IDF_VER);
 
 }
