e25a202d3cd69d0760e1a6525b823d73bfdad74d
Breno <engenheiro.brenoguimaraes@gmail.com>
2026-05-13 10:55:46 -0300

    atualização

--- Projeto_Breno_Vinicius/main/main.c
+++ Projeto_Breno_Vinicius/main/main.c
@@ -7,8 +7,11 @@
 // PRÁTICA 4  
 // Objetivo: fazer um controle PWM utilizando 3 botões por meio de interrupções, timers, filas e semáforos.
 
-
-
+ /*
+ * SPDX-FileCopyrightText: 2022-2023 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 #include <stdio.h>
 #include <inttypes.h>
 #include <stdlib.h>
@@ -25,6 +28,10 @@
 #include "driver/gptimer.h"
 #include "driver/ledc.h"
 #include "esp_err.h"
+#include "soc/soc_caps.h"
+#include "esp_adc/adc_oneshot.h"
+#include "esp_adc/adc_cali.h"
+#include "esp_adc/adc_cali_scheme.h"
 
 
 #define BOTAO1    21
@@ -39,6 +46,7 @@
 static const char* TAG = "boot";
 static const char* TAG2 = "botoes";
 static const char* TAG3 = "RELOGIO";
+const static char *TAG = "EXAMPLE";
 
 static QueueHandle_t gpio_evt_queue = NULL;
 QueueHandle_t fila_contador = NULL;
@@ -108,25 +116,25 @@ static void gpio_task_example(void* arg)
 
     for (;;) {
         if (xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
-           // int level = gpio_get_level(io_num);
-        //   *  if(io_num == BOTAO1) {
-        //         gpio_set_level(LED, 1);
-        //         ESP_LOGI(TAG2, "Botao 1 pressionado");
+            int level = gpio_get_level(io_num);
+           *  if(io_num == BOTAO1) {
+                 gpio_set_level(LED, 1);
+                 ESP_LOGI(TAG2, "Botao 1 pressionado");
                 
-        //     }
-        //     else if(io_num == BOTAO2) {
-        //         gpio_set_level(LED, 0);
-        //         ESP_LOGI(TAG2, "Botao 2 pressionado");
-        //     }
-        //     else if(io_num == BOTAO3) {
-        //         LED_STATE = !LED_STATE;
-        //         gpio_set_level(LED, LED_STATE);
-        //         ESP_LOGI(TAG2, "Botao 3 pressionado");
-        //     }
+             }
+             else if(io_num == BOTAO2) {
+                 gpio_set_level(LED, 0);
+               ESP_LOGI(TAG2, "Botao 2 pressionado");
+             }
+             else if(io_num == BOTAO3) {
+                 LED_STATE = !LED_STATE;
+                 gpio_set_level(LED, LED_STATE);
+                 ESP_LOGI(TAG2, "Botao 3 pressionado");
+             }
 
         }
 
-        //xQueueSendFromISR(fila_pwm, &io_num, NULL);
+        xQueueSendFromISR(fila_pwm, &io_num, NULL);
 
     }
 }
@@ -263,12 +271,13 @@ static void pwm_task(void* arg)
         .duty           = 0, // Set duty to 0%
         .hpoint         = 0
     };
-
+    ESP_ERROR_CHECK(ledc_channel_config(&led_channel));
+    ESP_ERROR_CHECK(ledc_channel_config(&osciloscopio_channel))
+    
     for (;;) {
     xSemaphoreTake(semaphore_pwm, portMAX_DELAY);
-    ESP_ERROR_CHECK(ledc_channel_config(&led_channel));
-    ESP_ERROR_CHECK(ledc_channel_config(&osciloscopio_channel));
-    xQueueReceive(fila_pwm, &duty, portMAX_DELAY);
+    xQueueReceive(fila_pwm, &duty, 10);
+    
 
     if (duty == 1){
         manual = false;
@@ -290,8 +299,158 @@ static void pwm_task(void* arg)
 
     }
 }   
+//----------------------- ADC -------------------------------
+static void ADC_task(void* arg)
+{
+/*---------------------------------------------------------------
+        ADC General Macros
+---------------------------------------------------------------*/
+//ADC1 Channels
+#define EXAMPLE_ADC1_CHAN0          32;
 
 
+#if (SOC_ADC_PERIPH_NUM >= 2) && !CONFIG_IDF_TARGET_ESP32C3
+/**
+ * On ESP32C3, ADC2 is no longer supported, due to its HW limitation.
+ * Search for errata on espressif website for more details.
+ */
+#define EXAMPLE_USE_ADC2            1
+#endif
+
+#if EXAMPLE_USE_ADC2
+//ADC2 Channels
+#define EXAMPLE_ADC2_CHAN0          ADC_CHANNEL_0
+#endif  //#if EXAMPLE_USE_ADC2
+
+#define EXAMPLE_ADC_ATTEN           ADC_ATTEN_DB_12
+
+static int adc_raw[2][10];
+static int voltage[2][10];
+static bool example_adc_calibration_init(adc_unit_t unit, adc_channel_t channel, adc_atten_t atten, adc_cali_handle_t *out_handle);
+static void example_adc_calibration_deinit(adc_cali_handle_t handle);
+
+void app_main(void)
+{
+    //-------------ADC1 Init---------------//
+    adc_oneshot_unit_handle_t adc1_handle;
+    adc_oneshot_unit_init_cfg_t init_config1 = {
+        .unit_id = ADC_UNIT_1,
+    };
+    ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config1, &adc1_handle));
+
+    //-------------ADC1 Config---------------//
+    adc_oneshot_chan_cfg_t config = {
+        .atten = EXAMPLE_ADC_ATTEN,
+        .bitwidth = ADC_BITWIDTH_DEFAULT, //resolução maxima 12 bits
+    };
+    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, EXAMPLE_ADC1_CHAN0, &config));//configura o canal 0 do ADC1
+
+    //-------------ADC1 Calibration Init---------------//
+    adc_cali_handle_t adc1_cali_chan0_handle = NULL;
+    adc_cali_handle_t adc1_cali_chan1_handle = NULL;
+    bool do_calibration1_chan0 = example_adc_calibration_init(ADC_UNIT_1, EXAMPLE_ADC1_CHAN0, EXAMPLE_ADC_ATTEN, &adc1_cali_chan0_handle);
+    bool do_calibration1_chan1 = example_adc_calibration_init(ADC_UNIT_1, EXAMPLE_ADC1_CHAN1, EXAMPLE_ADC_ATTEN, &adc1_cali_chan1_handle);
+
+
+    while (1) {
+        ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, EXAMPLE_ADC1_CHAN0, &adc_raw[0][0]));
+        ESP_LOGI(TAG, "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT_1 + 1, EXAMPLE_ADC1_CHAN0, adc_raw[0][0]);
+        
+        if (do_calibration1_chan0) {
+            ESP_ERROR_CHECK(adc_cali_raw_to_voltage(adc1_cali_chan0_handle, adc_raw[0][0], &voltage[0][0]));
+            ESP_LOGI(TAG, "ADC%d Channel[%d] Cali Voltage: %d mV", ADC_UNIT_1 + 1, EXAMPLE_ADC1_CHAN0, voltage[0][0]);
+        }
+        vTaskDelay(pdMS_TO_TICKS(1000));
+
+        ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, EXAMPLE_ADC1_CHAN1, &adc_raw[0][1]));
+        ESP_LOGI(TAG, "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT_1 + 1, EXAMPLE_ADC1_CHAN1, adc_raw[0][1]);
+        if (do_calibration1_chan1) {
+            ESP_ERROR_CHECK(adc_cali_raw_to_voltage(adc1_cali_chan1_handle, adc_raw[0][1], &voltage[0][1]));
+            ESP_LOGI(TAG, "ADC%d Channel[%d] Cali Voltage: %d mV", ADC_UNIT_1 + 1, EXAMPLE_ADC1_CHAN1, voltage[0][1]);
+        }
+        vTaskDelay(pdMS_TO_TICKS(1000));
+
+
+    }
+
+    //Tear Down
+    ESP_ERROR_CHECK(adc_oneshot_del_unit(adc1_handle));
+    if (do_calibration1_chan0) {
+        example_adc_calibration_deinit(adc1_cali_chan0_handle);
+    }
+    if (do_calibration1_chan1) {
+        example_adc_calibration_deinit(adc1_cali_chan1_handle);
+    }
+
+
+}
+
+/*---------------------------------------------------------------
+        ADC Calibration
+---------------------------------------------------------------*/
+static bool example_adc_calibration_init(adc_unit_t unit, adc_channel_t channel, adc_atten_t atten, adc_cali_handle_t *out_handle)
+{
+    adc_cali_handle_t handle = NULL;
+    esp_err_t ret = ESP_FAIL;
+    bool calibrated = false;
+
+#if ADC_CALI_SCHEME_CURVE_FITTING_SUPPORTED
+    if (!calibrated) {
+        ESP_LOGI(TAG, "calibration scheme version is %s", "Curve Fitting");
+        adc_cali_curve_fitting_config_t cali_config = {
+            .unit_id = unit,
+            .chan = channel,
+            .atten = atten,
+            .bitwidth = ADC_BITWIDTH_DEFAULT,
+        };
+        ret = adc_cali_create_scheme_curve_fitting(&cali_config, &handle);
+        if (ret == ESP_OK) {
+            calibrated = true;
+        }
+    }
+#endif
+
+#if ADC_CALI_SCHEME_LINE_FITTING_SUPPORTED
+    if (!calibrated) {
+        ESP_LOGI(TAG, "calibration scheme version is %s", "Line Fitting");
+        adc_cali_line_fitting_config_t cali_config = {
+            .unit_id = unit,
+            .atten = atten,
+            .bitwidth = ADC_BITWIDTH_DEFAULT,
+        };
+        ret = adc_cali_create_scheme_line_fitting(&cali_config, &handle);
+        if (ret == ESP_OK) {
+            calibrated = true;
+        }
+    }
+#endif
+
+    *out_handle = handle;
+    if (ret == ESP_OK) {
+        ESP_LOGI(TAG, "Calibration Success");
+    } else if (ret == ESP_ERR_NOT_SUPPORTED || !calibrated) {
+        ESP_LOGW(TAG, "eFuse not burnt, skip software calibration");
+    } else {
+        ESP_LOGE(TAG, "Invalid arg or no memory");
+    }
+
+    return calibrated;
+}
+
+static void example_adc_calibration_deinit(adc_cali_handle_t handle)
+{
+#if ADC_CALI_SCHEME_CURVE_FITTING_SUPPORTED
+    ESP_LOGI(TAG, "deregister %s calibration scheme", "Curve Fitting");
+    ESP_ERROR_CHECK(adc_cali_delete_scheme_curve_fitting(handle));
+
+#elif ADC_CALI_SCHEME_LINE_FITTING_SUPPORTED
+    ESP_LOGI(TAG, "deregister %s calibration scheme", "Line Fitting");
+    ESP_ERROR_CHECK(adc_cali_delete_scheme_line_fitting(handle));
+#endif
+}
+
+}
+//----------------------- Main -------------------------------
 void app_main(void)
 {
 
@@ -331,10 +490,11 @@ void app_main(void)
     
     // Criação do semáforo
     semaphore_pwm = xSemaphoreCreateBinary();
+    semaphore_ADC = xSemaphoreCreateBinary();
 
     // xTaskCreate(gpio_task_example, "gpio_task_example", 2048, NULL, 10, NULL);
     xTaskCreate(timer_task, "Tarefa para o timer", 4096, NULL, 10, NULL);
-
+    xTaskCreate(ADC_task, "Tarefa para o ADC", 4096, NULL, 10, NULL);
 }
 
 
