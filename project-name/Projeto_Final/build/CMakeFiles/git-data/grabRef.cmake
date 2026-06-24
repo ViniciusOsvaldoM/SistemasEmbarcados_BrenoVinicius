e0c1520fdf3a0f11adf145c4651c1ca59049263d
Breno <engenheiro.brenoguimaraes@gmail.com>
2026-06-24 10:36:09 -0300

    projto final

--- project-name/Projeto_Final/build/CMakeFiles/git-data/grabRef.cmake
+++ project-name/Projeto_Final/build/CMakeFiles/git-data/grabRef.cmake
@@ -18,7 +18,7 @@ set(HEAD_HASH)
 file(READ "C:/esp/SistemasEmbarcados_BrenoVinicius/project-name/Projeto_Final/build/CMakeFiles/git-data/HEAD" HEAD_CONTENTS LIMIT 1024)
 
 string(STRIP "${HEAD_CONTENTS}" HEAD_CONTENTS)
-set(GIT_DIR "C:/Users/Public/ESP/dist/v5.5.3/esp-idf/.git")
+set(GIT_DIR "C:/esp/SistemasEmbarcados_BrenoVinicius/.git")
 # handle git-worktree
 if(EXISTS "${GIT_DIR}/commondir")
 	file(READ "${GIT_DIR}/commondir" GIT_DIR_NEW LIMIT 1024)
